480273de33d7603ab8c9542ece0565db8ed65556
egorz734 <egorz734@mail.ru>
2021-05-30 05:33:24 +0300

    DEV: Server html parser

--- script2.py
+++ script2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pyodbc 
 
 path = 'data.htm'
 with open(path) as f:
@@ -12,3 +13,17 @@ data1 = data1[9:][0]
 names = data1.drop_duplicates().drop([11, 12])
 table2 = data[9:].dropna()
 table2[0] = names.values
+
+server = 'c2-185-12-28-165.elastic.cloud.croc.ru,1433' 
+database = 'db_Team7' 
+username = 'Team7' 
+password = 'Team71!ijn' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+for row in table2[1:].iterrows():
+    inf = ', '.join([f"'{row[1][i]}'" for i in range(9)])
+    cursor.execute(f"INSERT INTO RTDM_TECH.METRICS VALUES({inf})")
+cnxn.commit()
+
+cursor.close()
+cnxn.close()
