d2bf12b422e28a6f9ee5a8e802a8ca726592f166
egorz734 <egorz734@mail.ru>
2021-05-30 04:07:18 +0300

    Initial commit

---
+++ script2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+path = 'data1.htm'
+with open(path) as f:
+    data = pd.read_html(f)
+data = data[0]
+
+table1 = data[0:8][[7, 8]].dropna().drop([0, 1])
+
+data1 = data.fillna(0)
+data1 = data1[9:][0]
+names = data1.drop_duplicates().drop([11, 12])
+table2 = data[9:].dropna()
+table2[0] = names.values
